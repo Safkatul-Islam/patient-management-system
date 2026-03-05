f2a113bb152c7afb2ce7d3c7bc1a61a39c33b31f
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-03-04 20:56:55 -0800

    add patient microservice with in memory h2 database

---
+++ patient-service/src/main/java/org/pms/patientservice/model/Patient.java
@@ -0,0 +1,41 @@
+package org.pms.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "patients")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+
+}
